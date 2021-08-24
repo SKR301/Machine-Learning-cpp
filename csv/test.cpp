8c2f390a5cbec3ce8c1c53a6ffed09269dbf405d
Saurav KumaR <chunnu.bihat@gmail.com>
2021-08-25 00:09:55 +0530

    added function to read a table
    will be helpful for Apriori algorithm

--- csv/test.cpp
+++ csv/test.cpp
@@ -4,17 +4,25 @@
 int main(int argc, char const *argv[])
 {
 	csv CSV;
-	std::vector<std::vector<double>> result = CSV.read("data.csv");		//data file
+	std::vector<std::vector<double>> resultPoint = CSV.readPoint("dataPoint.csv");		//data file
 
 	std::cout<<"X: ";
-	for(int a=0;a<result.size();a++){
-		std::cout<<result[a][0]<<" ";
+	for(int a=0;a<resultPoint.size();a++){
+		std::cout<<resultPoint[a][0]<<" ";
 	}
 
 	std::cout<<"\n\n";
 	std::cout<<"Y: ";
-	for(int a=0;a<result.size();a++){
-		std::cout<<result[a][1]<<" ";
+	for(int a=0;a<resultPoint.size();a++){
+		std::cout<<resultPoint[a][1]<<" ";
+	}
+
+	std::vector<std::string> resultTable = CSV.readTable("dataTable.csv");		//data file
+
+	std::cout<<"\n\n";
+	std::cout<<"Table Content: ";
+	for(int a=0;a<resultTable.size();a++){
+		std::cout<<resultTable[a]<<" ";
 	}
 
 	std::cout<<"\n\n";
