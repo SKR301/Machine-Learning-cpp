0ba74a6885d3ce8f636ae7af420f795a62de1c46
Saurav KumaR <chunnu.bihat@gmail.com>
2021-11-16 23:53:34 +0530

    runAlgo completed #4

--- naive bayes/naive bayes.h
+++ naive bayes/naive bayes.h
@@ -6,8 +6,9 @@
 class NaiveBayes{
 private:
 	std::map<std::string, std::vector<int>> matrix;
-	int Ones;
-	int Zeros;
+	double Ones;
+	double Zeros;
+	double Totals;
 	std::string filename;
 
 public:
@@ -15,7 +16,7 @@ public:
 
 	void readData();		//read data from csv file
 
-	void runAlgo();			//predict outcome
+	void runAlgo(std::string);			//predict outcome
 
 	void displayMatrix();	//display matrix
 };
@@ -24,6 +25,7 @@ NaiveBayes::NaiveBayes(std::string dataFile){
 	filename=dataFile;
 	Ones=0;
 	Zeros=0;
+	Totals=0;
 }
 
 void NaiveBayes::readData(){
@@ -48,19 +50,39 @@ void NaiveBayes::readData(){
 		} else {
 			if(data[a][1] == "yes"){
 				++matrix[data[a][0]][1];
+				Ones++;
 			} else{
 				++matrix[data[a][0]][0];
+				Zeros++;
 			}
 		}
 	}
+
+	int total = Ones + Zeros;
 }
 
-void NaiveBayes::runAlgo(){
+void NaiveBayes::runAlgo(std::string key){
+	int total = Ones + Zeros;
 
+	double P_key_1 = matrix[key][1] / Ones;
+	double P_key_0 = matrix[key][0] / Zeros;
+
+	double P_1 = Ones / (Ones + Zeros);
+	double P_0 = Zeros / (Ones + Zeros);
+
+	double P_key = (matrix[key][1] + matrix[key][0]) / (Ones + Zeros);
+
+	double P_0_key = (P_key_0 * P_0)/(P_key);
+	double P_1_key = (P_key_1 * P_1)/(P_key);
+
+	std::cout<<"\n1 => "<<P_1_key<<"\t0 => "<<P_0_key;							//Print 1/0 probablity
+
+	std::cout<<"\n\n\nFINAL PREDICTION:";
+	(P_1_key > P_0_key)?std::cout<<" 1":std::cout<<" 0";						//Print prediction
 }	
 
 void NaiveBayes::displayMatrix(){
-	std::cout<<"\n\n---MATRIX---\n\n";
+	std::cout<<"\n\nMATRIX\n\n";
 	std::map<std::string, std::vector<int>>::iterator it = matrix.begin();
 
 	std::cout<<"Index"<<"\t\t"<<"1s"<<"\t\t"<<"0s"<<"\n";
--- naive bayes/test.cpp
+++ naive bayes/test.cpp
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[])
 	NaiveBayes NB("data.csv");
 	
 	NB.readData();
+	NB.runAlgo("rainy");					// predict outcome as 1/0 for given "key" parameter
 	NB.displayMatrix();
 
 	std::cout<<"\n\n";
