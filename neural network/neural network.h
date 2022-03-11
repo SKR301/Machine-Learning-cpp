34b28b3edf17474a834dd964fc82f305da5a4519
SKR301 <chunnu.bihat@gmail.com>
2022-03-11 17:00:32 +0530

    #6 updated skeleton for neural Network

--- neural network/neural network.h
+++ neural network/neural network.h
@@ -6,6 +6,36 @@ private:
 	std::vector<Neuron> inputLayer;
 	std::vector<std::vector<Path>> pathInputOutput;
 	std::vector<Neuron> outputLayer;
-public:
+	int output;
 	
+	int inputLayerSize;
+	int outputLayerSize;
+	int pathInputOutputSize;
+
+public:
+	void NeuralNetwork(std::vector<std::vector<int>>);
+	void setInputLayer(std::vector<std::vector<int>>);
+	void run();
+	int retOutput();
 };
+
+void NeuralNetwork::NeuralNetwork(std::vector<std::vector<int>> input){
+	int inputLayerSize = input.size() * input[0].size();
+	int outputLayerSize = 2;
+	int pathInputOutputSize = inputLayerSize * outputLayerSize;
+}
+
+void NeuralNetwork::setInputLayer(std::vector<std::vector<int>> input){
+	/*Feed data from input matrix to inputLayer*/
+}
+
+void NeuralNetwork::run(){
+	/*Run the network*/
+}
+
+int NeuralNetwork::retOutput(){
+	return output;
+}
+
+
+
