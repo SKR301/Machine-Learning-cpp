59c9e6d9cac1385d2ca27fecea564cc88cd4e440
SKR301 <chunnu.bihat@gmail.com>
2022-03-12 13:40:21 +0530

    #6 refresh start

--- neural network/test.cpp
+++ neural network/test.cpp
@@ -3,12 +3,7 @@
 
 int main(int argc, char const *argv[]){
 
-	std::vector<std::vector<int>> matrix {{1,0,0},{0,1,0},{0,0,1}};
-
-	NeuralNetwork NN(matrix);
-	NN.run();
-
-
+	
 	std::cout<<"\n\n";
 	system("pause");
 	return 0;
